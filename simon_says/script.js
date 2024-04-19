26fb1396caffcf461e9eb78c3f659d5bffd20c63
GastyBenitez <gastonbenitez0207@gmail.com>
2024-04-19 12:47:04 -0300

    inicio simon dice

---
+++ simon_says/script.js
@@ -0,0 +1,15 @@
+const round = document.getElementById('round');
+const simonButtons = document.getElementsByClassName('square');
+const startButton = document.getElementById('startButton');
+
+class Simon {
+    constructor(simonButtons, startButton, round){
+        this.round = 0;
+        this.userPosition = 0;
+        this.totalRounds = 10;
+        this.sequence = [];
+        this.speed = 1000;
+        this.blockedButtons = true;
+        this.buttons = Array.from(simonButtons);
+    }
+}
